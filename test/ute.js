c8df5e259359e21a6a69ebc8473e0101144df463
Cliffano Subagio <cliffano@gmail.com>
2014-11-06 17:28:48 +1100

    Remove optional express arg, use proxyquire instead. Improve test coverage to 100%.

--- test/ute.js
+++ test/ute.js
@@ -1,12 +1,13 @@
-var buster   = require('buster-node');
-var ejs      = require('ejs');
-var express  = require('express');
-var partials = require('express-partials');
-var fs       = require('fs');
-var nconf    = require('nconf');
-var referee  = require('referee');
-var Ute      = require('../lib/ute');
-var assert   = referee.assert;
+var buster     = require('buster-node');
+var ejs        = require('ejs');
+var express    = require('express');
+var partials   = require('express-partials');
+var fs         = require('fs');
+var nconf      = require('nconf');
+var proxyquire = require('proxyquire');
+var referee    = require('referee');
+var Ute        = require('../lib/ute');
+var assert     = referee.assert;
 
 buster.testCase('ute - ute', {
   setUp: function () {
@@ -42,15 +43,7 @@ buster.testCase('ute - ute', {
 
 buster.testCase('ute - start', {
   setUp: function () {
-    this.mockConsole = this.mock(console);
-    this.mockFs      = this.mock(fs);
-    this.mockNconf   = this.mock(nconf);
-  },
-  'should start the application on specified port': function () {
-
-    var self = this;
-
-    var mockApp = {
+    this.mockApp = {
       use   : function () {},
       engine: function (ext, type) {
         assert.equals(ext, '.html');
@@ -64,12 +57,22 @@ buster.testCase('ute - start', {
         assert.isTrue(handler !== null);
       }
     };
+    var self = this;
     var mockExpress = function () {
-      return mockApp;
+      return self.mockApp;
     };
     mockExpress.static = function (dir) {
       assert.equals(dir, 'somestaticdir');
     };
+    this.Ute = proxyquire('../lib/ute', { express: mockExpress });
+
+    this.mockConsole = this.mock(console);
+    this.mockFs      = this.mock(fs);
+    this.mockNconf   = this.mock(nconf);
+  },
+  'should start the application on specified port': function () {
+
+    var self = this;
     var handlers = {
       somehandler: function () {}
     };
@@ -81,9 +84,9 @@ buster.testCase('ute - start', {
     this.mockFs.expects('readFileSync').withExactArgs('test/fixtures/routes.json')
         .returns('[{ "method": "GET", "path": "/foo", "handler": "somehandler" }]');
 
-    var ute = new Ute({ confDir: 'test/fixtures', staticDir: 'somestaticdir' });
-    var app = ute.start(handlers, { express: mockExpress });
+    var ute = new this.Ute({ confDir: 'test/fixtures', staticDir: 'somestaticdir' });
+    var app = ute.start(handlers);
 
-    assert.equals(app, mockApp);
+    assert.equals(app, this.mockApp);
   }
 });
